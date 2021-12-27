ff61acdb7cb6701d72a9d4a2c64f13b8cd198ad9
Artemii Pudovkin <tyumen-777@yandex.ru>
2021-12-27 23:24:09 +0500

    Выполнено второе задание

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@
 import './App.css';
 import Profile from "./components/Profile";
 import ProfileClass from "./components/ProfileClass";
+import AuthorizationForm from "./components/AuthorizationForm";
 
 function App() {
 
@@ -9,6 +10,7 @@ function App() {
    <>
        <Profile name="Вася Пупкин" registredAt={new Date(2021, 5, 22)} />
        <ProfileClass name="Вася Пупкин" registredAt={new Date(2021, 5, 22)}/>
+       <AuthorizationForm/>
    </>
   );
 }
