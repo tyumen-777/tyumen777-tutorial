4eb2eec2af6da15e37cb6afdf0d2cda423f8b258
Artemii Pudovkin <tyumen-777@yandex.ru>
2022-01-09 23:36:13 +0500

    feat: Реализация формы отправки отзыва

--- src/components/Feedback.js
+++ src/components/Feedback.js
@@ -6,29 +6,39 @@ const styles = {
     display: "flex",
     flexDirection: "column",
     maxWidth: "300px",
-    textAlign: "center"
+    textAlign: "center",
+    margin: "0 auto",
   },
   form: {
     display: "flex",
     flexDirection: "column",
     maxWidth: "300px",
-    margin: "0 auto",
-  }
+  },
 };
 
 const Feedback = () => {
+  const [name, setName] = React.useState('');
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  };
+
+  const handleChange = (e) => {
+    setName(e.target.value)
+  }
+
   return (
     <div style={styles.wrapper}>
       <h2>Обратная связь:</h2>
-      <form style={styles.form}>
-        <TextField label="Имя" variant="outlined" type="text" margin="normal" />
+      <form style={styles.form} onSubmit={handleSubmit}>
+        <TextField label="Имя" variant="outlined" type="text" margin="normal" value={name} onChange={handleChange}/>
         <TextField label="Почта" type="email" margin="normal" />
         <TextField
-            label="Текст..."
-            type="text"
-            margin="normal"
-            multiline
-            rows="4"
+          label="Текст..."
+          type="text"
+          margin="normal"
+          multiline
+          rows="4"
         />
         <Button variant="contained" type="submit">
           Отправить
